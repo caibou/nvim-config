01b850de4d5af718e8e2771cc527478d100e1487
caibou <z.caibou@gmail.com>
2024-05-17 22:34:45 +0800

    [Modify] Change lsp hotkey

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -13,14 +13,15 @@ return {
         callback = function(ev)
           -- Buffer local mappings.
           -- See `:help vim.lsp.*` for documentation on any of the below functions
-          local opts = { buffer = ev.buf }
+          local opts = { buffer = ev.buf, noremap = true, nowait = true }
           local telescope_builtin = require('telescope.builtin')
 
           vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
           vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
-          -- vim.keymap.set('n', 'gr', telescope_builtin.lsp_references, opts)
-          vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-          vim.keymap.set('n', 'gI', telescope_builtin.lsp_implementations, opts)
+          vim.keymap.set('n', 'gr', telescope_builtin.lsp_references, opts)
+          vim.keymap.set('n', '<leader>gr', vim.lsp.buf.references, opts)
+          vim.keymap.set('n', 'gI', telescope_builtin.implementation, opts)
+          vim.keymap.set('n', '<leader>gI', vim.lsp.buf.implementation, opts)
 
           vim.keymap.set('n', '<leader>ds', telescope_builtin.lsp_document_symbols, { desc = '[D]ocument [S]ymbols' })
           vim.keymap.set('n', '<leader>ws', telescope_builtin.lsp_dynamic_workspace_symbols,
@@ -34,7 +35,7 @@ return {
             print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
           end, opts)
 
-          vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+          vim.keymap.set('n', '<leader>h', vim.lsp.buf.hover, opts)
           vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
           vim.keymap.set('i', '<C-h>', vim.lsp.buf.signature_help, opts)
           vim.keymap.set({ 'n', 'i' }, "<C-b>", function()
@@ -56,7 +57,6 @@ return {
   {
     "j-hui/fidget.nvim",
     config = function()
-      -- some tooltip for the lsp in bottom right
       require('fidget').setup({})
     end
   },
