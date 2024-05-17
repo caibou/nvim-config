c8a0bb18f2b75233d37b21b668212cac9ed032bd
caibou <z.caibou@gmail.com>
2024-05-17 22:31:53 +0800

    [Feature] Flutter add toggle log function

--- lua/plugins/flutter.lua
+++ lua/plugins/flutter.lua
@@ -10,22 +10,28 @@ local preview_stack_trace = function()
   end
 end
 
+function toggle_log()
+  local wins = vim.api.nvim_list_wins()
 
+  for _, id in pairs(wins) do
+    local bufnr = vim.api.nvim_win_get_buf(id)
+    if vim.api.nvim_buf_get_name(bufnr):match '.*/([^/]+)$' == '__FLUTTER_DEV_LOG__' then
+      return vim.api.nvim_win_close(id, true)
+    end
+  end
 
--- 检查是否安装了Telescope插件
-local telescope_installed = pcall(require, "telescope")
--- 如果Telescope插件可用，则加载flutter扩展
-if telescope_installed then
-  require("telescope").load_extension("flutter")
-  vim.keymap.set('n', '<leader>fl', require('telescope').extensions.flutter.commands, { noremap = true, silent = true })
+  pcall(function()
+    vim.api.nvim_command 'sb + __FLUTTER_DEV_LOG__ | resize 15 | wincmd J'
+  end)
 end
 
-vim.keymap.set('n', '<F5>', ':lua run_flutter()<CR>', { noremap = true, silent = true })
+
 
 -- 定义执行FlutterRun命令的函数
 function run_flutter()
   vim.cmd("FlutterRun --dart-define-from-file=launch_config_dev.json")
 end
+
 
 return {
   'akinsho/flutter-tools.nvim',
@@ -83,5 +89,19 @@ return {
       },
       root_patterns = { "pubspec.yaml" },
     })
+
+    local telescope_name = 'telescope';
+
+    -- 检查是否安装了Telescope插件
+    local telescope_installed = pcall(require, telescope_name)
+    -- 如果Telescope插件可用，则加载flutter扩展
+    if telescope_installed then
+      local telescope = require(telescope_name)
+      telescope.load_extension("flutter")
+      vim.keymap.set('n', '<leader>fl', telescope.extensions.flutter.commands, { noremap = true, silent = true })
+    end
+
+    vim.keymap.set('n', '<F5>', ':lua run_flutter()<CR>', { noremap = true, silent = true })
+    vim.keymap.set('n', '<leader>lg', ':lua toggle_log()<CR>', { noremap = true, silent = true })
   end,
 }
